7934c85750aba9c36f64364f05a01960a8a8e407
Artem <artem.yurasov.2003@mail.ru>
2022-03-13 21:34:49 +0300

    алиса.добавление максимума

--- main.cpp
+++ main.cpp
@@ -10,5 +10,9 @@ cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n';
+     if(a>b)
+        cout<<a;
+     else
+        cout<<b;
     return 0;
 }
